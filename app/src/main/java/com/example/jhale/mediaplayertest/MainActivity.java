19299fc8403b13bd9246a39da6a041a3c58a7bbb
jhalep <jhalep2@gmail.com>
2017-09-03 08:47:28 -0500

    Add clickable TextView

--- app/src/main/java/com/example/jhale/mediaplayertest/MainActivity.java
+++ app/src/main/java/com/example/jhale/mediaplayertest/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.jhale.mediaplayertest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView playAudio;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        playAudio = (TextView)findViewById(R.id.play_audio);
+        playAudio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playAudio.setText("Playing audio!");
+            }
+        });
     }
 }
